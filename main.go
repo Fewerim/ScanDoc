e6832709599e056a0674d5bd4b3f9051f08dd90a
Fewerim <fewerim55@gmail.com>
2025-11-13 18:03:33 +0500

    first commit

--- main.go
+++ main.go
@@ -5,12 +5,5 @@ import (
 )
 
 func main() {
-	fmt.Print("Hello and welcome")
-
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
-	}
-
-	fmt.Println("Hello and welcome")
-	fmt.Println("Hello and welcome")
+	fmt.Print("Project in process...")
 }
