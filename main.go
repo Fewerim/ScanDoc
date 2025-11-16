23a43cb5aed84463f397cc149cfa19071fed2548
Fewerim <fewerim55@gmail.com>
2025-11-16 09:33:26 +0500

    Добавил пакет objects в пакет typesJSON, добавил сигнатуру Торг-12

--- main.go
+++ main.go
@@ -1,41 +1,5 @@
 package main
 
-import (
-	"log"
-	"proWeb/files"
-)
-
 func main() {
-	if !files.StorageExists() {
-		if err := files.CreateStorageJSON(); err != nil {
-			log.Fatal(err)
-		}
-	}
-	datas := []files.TestJson{}
-	data1 := files.TestJson{
-		Name:       "Danila",
-		Age:        25,
-		Profession: "Golang programmer",
-		Skills: []string{
-			"SQL",
-			"Kafka",
-			"Python",
-		},
-	}
-	data2 := files.TestJson{
-		Name:       "Danila",
-		Age:        25,
-		Profession: "Golang programmer",
-		Skills: []string{
-			"SQL",
-			"Kafka",
-			"Python",
-		},
-	}
 
-	datas = append(datas, data1, data2)
-	err := files.SaveFileToDirectory("jssdo", "УТП", datas)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
--- typesJSON/upd.go
+++ typesJSON/upd.go
@@ -1,110 +1,18 @@
 package typesJSON
 
+import "proWeb/typesJSON/objects"
+
 type Upd struct {
-	DocumentNumber  string             `json:"documentNumber"`
-	DocumentDate    string             `json:"documentDate"`
-	Status          int                `json:"status"`
-	Seller          SellerObj          `json:"seller"`
-	Consignor       ConsignorObj       `json:"consignor"`
-	Consignee       ConsigneeObj       `json:"consignee"`
-	PaymentDocument PaymentDocumentObj `json:"paymentDocument"`
-	Buyer           BuyerObj           `json:"buyer"`
-	Currency        CurrencyObj        `json:"currency"`
-	Items           []ItemObj          `json:"items"`
-	TotalAmount     TotalAmountObject  `json:"totalAmount"`
-	Signatures      SignaturesObj      `json:"signatures"`
-	AdditionalInfo  AdditionalInfoObj  `json:"additionalInfo"`
-}
-
-type SellerObj struct {
-	Name    string `json:"name"`
-	Address string `json:"address"`
-	Inn     string `json:"inn"`
-	Kpp     string `json:"kpp"`
-}
-
-type ConsignorObj struct {
-	Name    string `json:"name"`
-	Address string `json:"address"`
-}
-
-type ConsigneeObj struct {
-	Name    string `json:"name"`
-	Address string `json:"address"`
-}
-
-type PaymentDocumentObj struct {
-	Number string `json:"number"`
-	Date   string `json:"date"`
-}
-
-type BuyerObj struct {
-	Name    string `json:"name"`
-	Address string `json:"address"`
-	Inn     string `json:"inn"`
-	Kpp     string `json:"kpp"`
-}
-
-type CurrencyObj struct {
-	Name string `json:"name"`
-	Code string `json:"code"`
-}
-
-type ItemObj struct {
-	LineNumber              int     `json:"lineNumber"`
-	ProductCode             string  `json:"productCode"`
-	ProductName             string  `json:"productName"`
-	Unit                    string  `json:"unit"`
-	Quantity                float64 `json:"quantity"`
-	Price                   float64 `json:"price"`
-	AmountWithoutVAT        float64 `json:"amountWithoutVAT"`
-	ExciseAmount            float64 `json:"exciseAmount"`
-	VatRatePercent          int     `json:"vatRatePercent"`
-	VatAmount               float64 `json:"vatAmount"`
-	AmountWithVAT           float64 `json:"amountWithVAT"`
-	CountryOfOrigin         string  `json:"countryOfOrigin"`
-	CustomDeclarationNumber string  `json:"customDeclarationNumber"`
-	CustomCode              string  `json:"customCode"`
-	ShortName               string  `json:"shortName"`
-}
-
-type TotalAmountObject struct {
-	TotalWithoutVAT float64 `json:"totalWithoutVAT"`
-	TotalVAT        float64 `json:"totalVAT"`
-	TotalWithVAT    float64 `json:"totalWithVAT"`
-}
-
-type SignaturesObj struct {
-	AuthorizedPerson       AuthorizedPersonObj       `json:"authorizedPerson"`
-	ChiefAccountant        ChiefAccountantObj        `json:"chiefAccountant"`
-	IndividualEntrepreneur IndividualEntrepreneurObj `json:"individualEntrepreneur"`
-}
-
-type AuthorizedPersonObj struct {
-	Position string `json:"position"`
-	Name     string `json:"name"`
-}
-type ChiefAccountantObj struct {
-	Name string `json:"name"`
-}
-type IndividualEntrepreneurObj struct {
-	Name                string `json:"name"`
-	RegistrationDetails string `json:"registrationDetails"`
-}
-
-type AdditionalInfoObj struct {
-	ShipmentBasis      string                 `json:"shipmentBasis"`
-	TransportDocuments string                 `json:"transportDocuments"`
-	Notes              string                 `json:"notes"`
-	ResponsiblePerson  []ResponsiblePersonObj `json:"responsiblePerson"`
-	DocumentCreator    DocumentCreatorObj     `json:"documentCreator"`
-}
-type ResponsiblePersonObj struct {
-	Position string `json:"position"`
-	Name     string `json:"name"`
-}
-type DocumentCreatorObj struct {
-	Name string `json:"name"`
-	Inn  string `json:"inn"`
-	Kpp  string `json:"kpp"`
+	DocumentInfo    objects.DocumentInfoObj    `json:"document_info"`
+	Status          int                        `json:"status"`
+	Seller          objects.SellerObj          `json:"seller"`
+	Consignor       objects.ConsignorObj       `json:"consignor"`
+	Consignee       objects.ConsigneeObj       `json:"consignee"`
+	PaymentDocument objects.PaymentDocumentObj `json:"paymentDocument"`
+	Buyer           objects.BuyerObj           `json:"buyer"`
+	Currency        objects.CurrencyObj        `json:"currency"`
+	Items           []objects.ItemUpdObj       `json:"items"`
+	TotalAmount     objects.TotalAmountObject  `json:"totalAmount"`
+	Signatures      objects.SignaturesObj      `json:"signatures"`
+	AdditionalInfo  objects.AdditionalInfoObj  `json:"additionalInfo"`
 }
