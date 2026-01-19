7c278ca3a0c273b04b35fd1c576bd4a106d708f8
Fewerim <fewerim55@gmail.com>
2026-01-19 22:34:06 +0500

    добавил руководства

--- main.go
+++ main.go
@@ -1,15 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"proWeb/internal/appUtils"
-)
-
 func main() {
-	dir, err := appUtils.ConvertPdfToImg("C:\\Users\\Administrator\\Downloads\\Telegram Desktop\\4ТОРГ-12.pdf")
-	if err != nil {
-		fmt.Println("Ошибка:", err)
-		return
-	}
-	fmt.Println("JPG сохранены в:", dir)
 }
